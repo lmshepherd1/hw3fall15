0ed4434793d18db63e4ec43fc9e7da89ec0a69a3
lmshepherd1 <laura-shepherd@uiowa.edu>
2015-10-30 15:45:03 +0000

    homework complete

--- lib/oracle_of_bacon.rb
+++ lib/oracle_of_bacon.rb
@@ -20,13 +20,15 @@ class OracleOfBacon
   validate :from_does_not_equal_to
 
   def from_does_not_equal_to
- #   if @from == @to
- #     self.errors.add(:from, 'cannot be the same as To')
-  #  end
+    if @from == @to
+      self.errors.add(:from, 'cannot be the same as To')
+    end
   end
 
   def initialize(api_key='')
-    # your code here
+    @api_key = api_key
+    @from = 'Kevin Bacon'
+    @to = 'Kevin Bacon'
   end
 
   def find_connections
@@ -39,16 +41,18 @@ class OracleOfBacon
       # convert all of these into a generic OracleOfBacon::NetworkError,
       #  but keep the original error message
       # your code here
+      raise NetworkError
     end
     # your code here: create the OracleOfBacon::Response object
+    @response = Response.new(xml)
   end
 
   def make_uri_from_arguments
-    # your code here: set the @uri attribute to properly-escaped URI
-    #   constructed from the @from, @to, @api_key arguments
+    @uri = "http://oracleofbacon.org/cgi-bin/xml?p=" + @api_key + "&a=" + CGI.escape(@to) + "&b=" + CGI.escape(@from);
   end
       
   class Response
+    attr_accessor :type, :data
     attr_reader :type, :data
     # create a Response object from a string of XML markup.
     def initialize(xml)
@@ -59,16 +63,45 @@ class OracleOfBacon
     private
 
     def parse_response
+      # error
       if ! @doc.xpath('/error').empty?
         parse_error_response
-      # your code here: 'elsif' clauses to handle other responses
-      # for responses not matching the 3 basic types, the Response
-      # object should have type 'unknown' and data 'unknown response'         
+      # spellcheck 
+      elsif ! @doc.xpath('//spellcheck').empty?
+        parse_spellcheck_response
+      elsif ! @doc.xpath('//movie').empty? and ! @doc.xpath('//actor').empty? and @doc.xpath('//actor').length - @doc.xpath('//movie').length === 1
+        parse_graph_response
+      else
+        parse_unknown_response        
       end
     end
     def parse_error_response
       @type = :error
       @data = 'Unauthorized access'
+    end
+    def parse_spellcheck_response
+      @type = :spellcheck
+      @data = []
+      @doc.xpath('//match').each do |val|
+        @data.push(val.text)
+      end
+      # @data = @doc.xpath('//match')
+    end
+    def parse_graph_response
+      @type = :graph
+      @actors = []
+      @movies = []
+      @doc.xpath('//actor').each do |val|
+        @actors.push(val.text)
+      end
+      @doc.xpath('//movie').each do |val|
+        @movies.push(val.text)
+      end
+      @data = @actors.zip(@movies).flatten.compact
+    end
+    def parse_unknown_response
+      @type = :unknown
+      @data = 'unknown response'
     end
   end
 end
